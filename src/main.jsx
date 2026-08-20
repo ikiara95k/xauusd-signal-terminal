e4e6e9871828a9ad719c2e2e0aad65f164febd55
ikiara95k <kenanikiara95@gmail.com>
2026-08-20 18:59:47 +0300

    Add backtest route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import Backtest from "./Backtest.jsx";
 import "./styles.css";
+
+const isBacktest = new URLSearchParams(window.location.search).get("backtest") === "1";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {isBacktest ? <Backtest /> : <App />}
   </React.StrictMode>
 );
